b164ece6e9e17a499edd01ee4e4706b4422b6d35
Makhnev Pavel <pavel.makhnev@bk.ru>
2020-08-27 11:52:53 +0300

    .md

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -7,37 +7,37 @@
 
 if (!function_exists('apply_query_filters'))
 {
-	/**
-	 * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Query\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
-	 * @param array $filter
-	 */
-	function apply_query_filters($query, array $filter)
-	{
-		foreach ($filter as $key => $value){
-			if (!is_array($value)){
-				if($fromJson = json_decode($value)){
-					$value = $fromJson;
-				} else {
-					$value = [$value];
-				}
-			}
-			if(!is_array($value)){
-				$value = [$value];
-			}
-			if (!is_numeric($key)){
-				array_unshift($value, $key);
-			}
+    /**
+     * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Query\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
+     * @param array $filter
+     */
+    function apply_query_filters($query, array $filter)
+    {
+        foreach ($filter as $key => $value){
+            if (!is_array($value)){
+                if($fromJson = json_decode($value)){
+                    $value = $fromJson;
+                } else {
+                    $value = [$value];
+                }
+            }
+            if(!is_array($value)){
+                $value = [$value];
+            }
+            if (!is_numeric($key)){
+                array_unshift($value, $key);
+            }
 
-			if(isset($value[0]) && strpos($value[0], '.') !== false){
-				$exploded = explode('.', $value[0]);
-				$field = $exploded[count($exploded)-1];
-				$relation = $exploded;
-				unset($relation[count($exploded)-1]);
+            if(isset($value[0]) && strpos($value[0], '.') !== false){
+                $exploded = explode('.', $value[0]);
+                $field = $exploded[count($exploded)-1];
+                $relation = $exploded;
+                unset($relation[count($exploded)-1]);
 
-				$relation  = implode('.', $relation);
+                $relation  = implode('.', $relation);
 
-				$val = $value;
-				$val[0] = $field;
+                $val = $value;
+                $val[0] = $field;
 
                 if ($value[1] == 'in') {
                     $val2 = [];
@@ -51,13 +51,13 @@ if (!function_exists('apply_query_filters'))
                     continue;
                 }
 
-				$query->whereHas($relation, function($q) use ($val, $query, $relation) {
+                $query->whereHas($relation, function($q) use ($val, $query, $relation) {
                     $val[0] = $q->getModel()->getTable() . '.' . $val[0];
-					call_user_func_array([$q, 'where'], $val);
-				});
+                    call_user_func_array([$q, 'where'], $val);
+                });
 
-				continue;
-			}
+                continue;
+            }
 
             if ($value[1] == 'in') {
                 $val = [];
@@ -67,54 +67,106 @@ if (!function_exists('apply_query_filters'))
                 continue;
             }
 
-			call_user_func_array([$query, 'where'], $value);
-		}
-	}
+            call_user_func_array([$query, 'where'], $value);
+        }
+    }
 }
 
 if (!function_exists('apply_query_scopes'))
 {
-	/**
-	 * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
-	 * @param array $scopes
-	 */
-	function apply_query_scopes(&$query, array $scopes)
-	{
-		foreach ($scopes as $key => $value2){
-			$value = explode(',', $value2, 2);
-			if(mb_strtolower($value[0]) === 'withoutglobalscopes'){
-			    continue;
+
+    function str_between( $string,  $start,  $end, $includeDelimiters = false, &$offset = 0)
+    {
+        if ($string === '' || $start === '' || $end === '') return null;
+
+        $startLength = strlen($start);
+        $endLength = strlen($end);
+
+        $startPos = strpos($string, $start, $offset);
+        if ($startPos === false) return null;
+
+        $endPos = strpos($string, $end, $startPos + $startLength);
+        if ($endPos === false) return null;
+
+        $length = $endPos - $startPos + ($includeDelimiters ? $endLength : -$startLength);
+        if (!$length) return '';
+
+        $offset = $startPos + ($includeDelimiters ? 0 : $startLength);
+
+        $result = substr($string, $offset, $length);
+
+        return ($result !== false ? $result : null);
+    }
+
+    function str_between_all( $string,  $start,  $end,  $includeDelimiters = false,  &$offset = 0)
+    {
+        $strings = [];
+        $length = strlen($string);
+
+        while ($offset < $length)
+        {
+            $found = \str_between($string, $start, $end, $includeDelimiters, $offset);
+            if ($found === null) break;
+
+            $strings[] = $found;
+            $offset += strlen($includeDelimiters ? $found : $start . $found . $end); // move offset to the end of the newfound string
+        }
+
+        return $strings;
+    }
+
+
+    /**
+     * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
+     * @param array $scopes
+     */
+    function apply_query_scopes(&$query, array $scopes)
+    {
+//        dd(123);
+        foreach ($scopes as $key => $value2){
+
+            $value = explode(',', $value2, 2);
+            if(mb_strtolower($value[0]) === 'withoutglobalscopes'){
+                continue;
             }
 
-			$method = new ReflectionMethod(get_class($query->getModel()), 'scope'.ucfirst($value[0]));
-			$count = $method->getNumberOfParameters();
+            $method = new ReflectionMethod(get_class($query->getModel()), 'scope'.ucfirst($value[0]));
+            $count = $method->getNumberOfParameters();
 
+            $replaced =  str_between_all($value2, '[', ']', true);
+            foreach($replaced as $rep){
+                $value2 = str_replace($rep, str_replace(',', '||||||||++|', $rep), $value2);
+            }
             $value = explode(',', $value2, $count);
-			if (!method_exists($query->getModel(), 'scope'.ucfirst($value[0]))){
-				continue;
-			}
-			if(!isset($value[1])){
-				$value[1] = json_encode([]);
-			}
+            if (!method_exists($query->getModel(), 'scope'.ucfirst($value[0]))){
+                continue;
+            }
+
+            foreach($value as &$val){
+                $val = str_replace('||||||||++|', ',', $val);
+            }
+            if(!isset($value[1])){
+                $value[1] = json_encode([]);
+            }
 //			$query->{$value[0]}(json_decode($value[1], true));
             $method = $value[0];
-			array_shift($value);
+            array_shift($value);
 //			array_unshift($value, $query);
 
-			call_user_func_array([$query, $method], $value);
-		}
-		return $query;
-	}
+            call_user_func_array([$query, $method], $value);
+        }
+        return $query;
+    }
 }
 
 if (!function_exists('apply_query_with'))
 {
-	/**
-	 * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
-	 * @param array $with
-	 */
-	function apply_query_with($query, array $with)
-	{
-		$query->with((array) $with);
-	}
+    /**
+     * @param Illuminate\Database\Eloquent\Builder|Illuminate\Database\Eloquent\Relations\Relation $query
+     * @param array $with
+     */
+    function apply_query_with($query, array $with)
+    {
+        $query->with((array) $with);
+    }
 }
